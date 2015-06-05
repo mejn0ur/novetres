578a15fd827e019b78141dbbb425505d2d586072
mejn0ur <pina.eng.civil@gmail.com>
2015-06-05 01:53:10 -0300

    15/12/2012

---
+++ media_notas.py
@@ -0,0 +1,17 @@
+#este programa retorna a media aritmetica
+#de 5 notas no decorrer de um semestre.
+
+print 'Entre com a primeira nota:'
+p1 = raw_input('-> ')
+print 'Entre com a segunda nota:'
+p2 = raw_input('-> ')
+print 'Entre com a terceira nota:'
+p3 = raw_input('-> ')
+print 'Entre com a quarta nota:'
+p4 = raw_input('-> ')
+print 'Entre com a quinta e ultima nota:'
+p5 = raw_input('-> ')
+
+media = (p1+p2+p3+p4+p5)/5
+
+print 'A media das notas apresentadas e:', media
